e21e0455d1ee7260ccb8ce74c822efb0bebb8d12
gurofinance <guro.finance.data@gmail.com>
2022-08-22 14:07:34 +0900

    scaler complete

--- data_visualize.py
+++ data_visualize.py
@@ -1,9 +1,10 @@
 from ast import Pass
+import atexit
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder , StandardScaler ,MinMaxScaler , PowerTransformer
 
 
 class data_:
@@ -18,3 +19,33 @@ class data_:
             column_list.append(i)
         
         return column_list
+    
+    def drop_columns(self , df ,column):
+        return df.drop(column, axis=1)
+        
+    def StandardScale(self, df , target):
+        sc = StandardScaler()
+        x = df.drop(target, axis=1)
+        scaled_features = sc.fit_transform(x)
+        scaled_features_df = pd.DataFrame(scaled_features , index=x.index , columns=x.columns)
+        scaled_features_df[target] = df[target]
+        
+        return scaled_features_df 
+    
+    def MinMaxScale(self, df , target):
+        sc = MinMaxScaler()
+        x = df.drop(target, axis=1)
+        scaled_features = sc.fit_transform(x)
+        scaled_features_df = pd.DataFrame(scaled_features , index=x.index , columns=x.columns)
+        scaled_features_df[target] = df[target]
+        
+        return scaled_features_df
+    
+    def PowerScale(self, df , target):
+        sc = PowerTransformer()
+        x = df.drop(target, axis=1)
+        scaled_features = sc.fit_transform(x)
+        scaled_features_df = pd.DataFrame(scaled_features , index=x.index , columns=x.columns)
+        scaled_features_df[target] = df[target]
+        
+        return scaled_features_df
--- ml_gui_program.py
+++ ml_gui_program.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import *
 import sys , pickle
 from PyQt5 import uic , QtWidgets 
 from data_visualize import data_
-
+from table_display import DataFrameModel
 
 class UI(QMainWindow):
     def __init__(self):
@@ -15,9 +15,23 @@ class UI(QMainWindow):
         
         self.Browse = self.findChild(QPushButton , "Browse")
         self.columns = self.findChild(QListWidget , "listWidget")
+        self.table = self.findChild(QTableView , "tableView")
+        self.data_shape = self.findChild(QLabel , "shape")
+        self.Submit = self.findChild(QPushButton , "Submit")
+        self.target_name = self.findChild(QLabel , "target_name")
+        self.dropcolumn = self.findChild(QComboBox , "dropcolumn")
+        self.drop = self.findChild(QPushButton , "drop")
+        
+        self.scaler = self.findChild(QComboBox , "scaler")
+        self.scale_btn = self.findChild(QPushButton , "scale_btn")
+        
         
         self.Browse.clicked.connect(self.get_csv)
+        self.columns.clicked.connect(self.target)
+        self.Submit.clicked.connect(self.set_target)
+        self.drop.clicked.connect(self.dropc)
         
+        self.scale_btn.clicked.connect(self.scale_value)
         # self.show()
     def filldetails(self, fleg = 1):
         if fleg == 0:
@@ -28,15 +42,50 @@ class UI(QMainWindow):
         # print(self.column_list)
         
         for i , j in enumerate(self.column_list):
-            stri = f'{j}------{str(self.df[j].dtype)}'
+            stri = f'{j} ------ {str(self.df[j].dtype)}'
             print(stri)
             self.columns.insertItem(i , stri)
         
+        x , y  = self.df.shape
+        self.data_shape.setText(f'({x} , {y})')
+        self.fill_combo_box()
         
+    def fill_combo_box(self):
         
+        self.dropcolumn.clear()
+        self.dropcolumn.addItems(self.column_list)
+        x = DataFrameModel(self.df)
+        self.table.setModel(x)
+        
+    def target(self):
+        self.item = self.columns.currentItem().text().split(' ')[0]
+        print(self.columns.currentItem().text().split(' ')[0])
     
+    def set_target(self):
+        self.target_value = self.item
+        self.target_name.setText(self.target_value)
+    
+    def dropc(self):
+        selected = self.dropcolumn.currentText()
+        self.df = data.drop_columns(self.df , selected)
+        self.filldetails()
+    
+    
+    
+    def scale_value(self):
+        if self.scaler.currentText() == "StandardScale":
+            self.df = data.StandardScale(self.df, self.target_value )
+        
+        elif self.scaler.currentText() == "MinMaxScale":
+            self.df = data.MinMaxScale(self.df, self.target_value )
+        
+        else :
+            self.df = data.PowerScale(self.df, self.target_value )
+            
+        self.filldetails()
+                
     def get_csv(self):
-        self.file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "", "csv(*.csv)")
+        self.file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "C:/apps/ml/datasets/", "csv(*.csv)")
         self.columns.clear()
         
         if self.file_path !="":
